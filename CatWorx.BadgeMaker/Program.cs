90400c04f24f9250e7d7eca6fe53d0eaf4c3d4a9
Xavier Marquez <xavier.ray.marquez@gmail.com>
2023-03-27 19:16:11 -0700

    getting randomly generated information from API and posting to badge

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -6,32 +6,32 @@ namespace CatWorx.BadgeMaker
 {
   class Program
   {
-  static List<Employee> GetEmployees()
-  {
-    List<Employee> employees = new List<Employee>();
-    while(true)
-    {
-      Console.WriteLine("Enter first name (leave empty to exit): ");
+  // static List<Employee> GetEmployees()
+  // {
+  //   List<Employee> employees = new List<Employee>();
+  //   while(true)
+  //   {
+  //     Console.WriteLine("Enter first name (leave empty to exit): ");
 
-      string firstName  = Console.ReadLine() ?? "";
-      // or if something else is added.
-      if (firstName  =="")
-      {
-        break;
-      }
+  //     string firstName  = Console.ReadLine() ?? "";
+  //     // or if something else is added.
+  //     if (firstName  =="")
+  //     {
+  //       break;
+  //     }
       
 
-      Console.Write("Enter last name: ");
-      string lastName = Console.ReadLine() ?? "";
-      Console.Write("Enter ID: ");
-      int id = Int32.Parse(Console.ReadLine() ?? "");
-      Console.Write("Enter Photo URL:");
-      string photoUrl = Console.ReadLine() ?? "";
-      Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
-      employees.Add(currentEmployee);
-    }
-      return employees;
-    }
+  //     Console.Write("Enter last name: ");
+  //     string lastName = Console.ReadLine() ?? "";
+  //     Console.Write("Enter ID: ");
+  //     int id = Int32.Parse(Console.ReadLine() ?? "");
+  //     Console.Write("Enter Photo URL:");
+  //     string photoUrl = Console.ReadLine() ?? "";
+  //     Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+  //     employees.Add(currentEmployee);
+  //   }
+  //     return employees;
+  //   }
 //   static void PrintEmployees(List<Employee> employees)
 //       {
 //         for (int i = 0; i < employees.Count; i++) 
@@ -43,7 +43,8 @@ namespace CatWorx.BadgeMaker
   async static Task Main(string[] args)
     {
       List<Employee> employees = new List<Employee>();
-      employees = GetEmployees();
+      // employees = GetEmployees();
+      employees = await PeopleFetcher.GetFromApi();
 
       Util.PrintEmployees(employees);
       Util.MakeCSV(employees);
